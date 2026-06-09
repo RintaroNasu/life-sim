f5bf4b791d28494f29c8ff1f2c0be24d0d123b6c
RintaroNasu <rinta022823@gmail.com>
2026-06-10 00:38:05 +0900

    feat: :zap: 新規登録画面の作成

--- web/lib/api/auth.ts
+++ web/lib/api/auth.ts
@@ -3,6 +3,12 @@ export type LoginRequest = {
   password: string;
 };
 
+export type SignupRequest = {
+  name: string;
+  email: string;
+  password: string;
+};
+
 export type LoginResponse = {
   token: string;
 };
@@ -35,6 +41,30 @@ export const login = async (
     throw new Error(
       data.error?.message ??
         "ログインに失敗しました。時間を置いて再度お試しください。",
+    );
+  }
+
+  return data;
+};
+
+export const signup = async (
+  input: SignupRequest,
+): Promise<LoginResponse & ApiErrorResponse> => {
+  const response = await fetch(`${API_BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(input),
+  });
+
+  const data = (await response.json()) as LoginResponse &
+    ApiErrorResponse;
+
+  if (!response.ok) {
+    throw new Error(
+      data.error?.message ??
+        "新規登録に失敗しました。時間を置いて再度お試しください。",
     );
   }
 
